81be6507e9c98e1a66495c848ce25d476a556d16
David <david@MacBook-Pro-de-David-2.local>
2020-07-14 18:30:01 +0200

    Heroku CORS get

--- src/main/java/com/backend/sprint/SecurityConfig.java
+++ src/main/java/com/backend/sprint/SecurityConfig.java
@@ -1,12 +1,14 @@
 package com.backend.sprint;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
+@EnableWebSecurity
 public class SecurityConfig implements WebMvcConfigurer {
 
 	@Override
