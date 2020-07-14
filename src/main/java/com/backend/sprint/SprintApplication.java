98e02858ef78c77f5f9f7a4c9454df3c68eccca5
David <david@MacBook-Pro-de-David-2.local>
2020-07-14 18:15:57 +0200

    Heroku CORS get

--- src/main/java/com/backend/sprint/SprintApplication.java
+++ src/main/java/com/backend/sprint/SprintApplication.java
@@ -1,14 +1,11 @@
 package com.backend.sprint;
 
-import java.util.Arrays;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableAutoConfiguration
@@ -68,17 +65,32 @@ public class SprintApplication {
 	// }
 
 	@Bean
-	CorsConfigurationSource corsConfigurationSource() {
-		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(Arrays.asList("https://club-sprint-front.herokuapp.com/*/*",
-				"https://club-sprint-front.herokuapp.com/*", "https://club-sprint-front.herokuapp.com",
-				"http://localhost:3000"));
-		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS"));
-		configuration.setAllowedHeaders(Arrays.asList("Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
-				"Access-Control-Request-Method", "Access-Control-Request-Headers", "Origin", "Cache-Control",
-				"Content-Type"));
-		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", configuration);
-		return source;
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/*").allowedOrigins("https://club-sprint-front.herokuapp.com");
+			}
+		};
 	}
+
+	// @Bean
+	// CorsConfigurationSource corsConfigurationSource() {
+	// CorsConfiguration configuration = new CorsConfiguration();
+	// configuration
+	// .setAllowedOrigins(Arrays.asList("https://club-sprint-front.herokuapp.com/*/*",
+	// "https://club-sprint-front.herokuapp.com/*",
+	// "https://club-sprint-front.herokuapp.com", "http://localhost:3000"));
+	// configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT",
+	// "DELETE", "HEAD", "OPTIONS"));
+	// configuration.setAllowedHeaders(Arrays.asList("Access-Control-Allow-Headers",
+	// "Access-Control-Allow-Origin",
+	// "Access-Control-Request-Method", "Access-Control-Request-Headers",
+	// "Origin", "Cache-Control",
+	// "Content-Type"));
+	// final UrlBasedCorsConfigurationSource source = new
+	// UrlBasedCorsConfigurationSource();
+	// source.registerCorsConfiguration("/**", configuration);
+	// return source;
+	// }
 }
