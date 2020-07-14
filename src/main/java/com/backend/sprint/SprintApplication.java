753cc6b8bc04ec7fc4555b4e3ec792c5e1d30f47
David <david@MacBook-Pro-de-David-2.local>
2020-07-14 17:46:39 +0200

    Heroku CORS get

--- src/main/java/com/backend/sprint/SprintApplication.java
+++ src/main/java/com/backend/sprint/SprintApplication.java
@@ -1,17 +1,8 @@
 package com.backend.sprint;
 
-import java.util.Arrays;
-import java.util.Collections;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.Ordered;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 @EnableAutoConfiguration
@@ -21,19 +12,21 @@ public class SprintApplication {
 		SpringApplication.run(SprintApplication.class, args);
 	}
 
-	@Bean
-	public FilterRegistrationBean<CorsFilter> simpleCorsFilter() {
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowCredentials(false);
-		config.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
-		config.setAllowedMethods(Collections.singletonList("*"));
-		config.setAllowedHeaders(Collections.singletonList("*"));
-		source.registerCorsConfiguration("/**", config);
-		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
-		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-		return bean;
-	}
+	// @Bean
+	// public FilterRegistrationBean<CorsFilter> simpleCorsFilter() {
+	// UrlBasedCorsConfigurationSource source = new
+	// UrlBasedCorsConfigurationSource();
+	// CorsConfiguration config = new CorsConfiguration();
+	// config.setAllowCredentials(false);
+	// config.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+	// config.setAllowedMethods(Collections.singletonList("*"));
+	// config.setAllowedHeaders(Collections.singletonList("*"));
+	// source.registerCorsConfiguration("/**", config);
+	// FilterRegistrationBean<CorsFilter> bean = new
+	// FilterRegistrationBean<>(new CorsFilter(source));
+	// bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+	// return bean;
+	// }
 
 	// @Bean
 	// public CorsFilter corsConfigurationSource() {
