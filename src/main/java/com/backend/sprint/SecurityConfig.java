23d2b2b90ad559accb643e68cbf8a9c1f49936f5
David <david@MacBook-Pro-de-David-2.local>
2020-07-14 16:59:21 +0200

    Heroku CORS get

--- src/main/java/com/backend/sprint/SecurityConfig.java
+++ src/main/java/com/backend/sprint/SecurityConfig.java
@@ -24,6 +24,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.applyPermitDefaultValues();
+		corsConfiguration.addAllowedMethod(HttpMethod.GET);
 		corsConfiguration.addAllowedMethod(HttpMethod.PUT);
 		corsConfiguration.addAllowedMethod(HttpMethod.DELETE);
 
