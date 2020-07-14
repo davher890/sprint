016c3ace7497f1e1bbfdaa82cce2f9ef6799190c
David <david@MacBook-Pro-de-David-2.local>
2020-07-14 17:51:27 +0200

    Heroku CORS get

--- src/main/java/com/backend/sprint/SprintApplication.java
+++ src/main/java/com/backend/sprint/SprintApplication.java
@@ -3,6 +3,9 @@ package com.backend.sprint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableAutoConfiguration
@@ -10,6 +13,16 @@ public class SprintApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SprintApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/*").allowedOrigins("*");
+			}
+		};
 	}
 
 	// @Bean
