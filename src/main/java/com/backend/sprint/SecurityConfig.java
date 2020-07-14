98adf35b6e989433a55e3c3071699bf8fc49096b
David <david@MacBook-Pro-de-David-2.local>
2020-07-14 17:35:23 +0200

    Heroku CORS get

--- src/main/java/com/backend/sprint/SecurityConfig.java
+++ src/main/java/com/backend/sprint/SecurityConfig.java
@@ -1,43 +1,45 @@
 package com.backend.sprint;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@EnableWebMvc
+public class SecurityConfig implements WebMvcConfigurer {
 
 	@Override
-	protected void configure(HttpSecurity httpSecurity) throws Exception {
-		httpSecurity.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().cors().and()
-				.csrf().disable().authorizeRequests().antMatchers("*").permitAll();
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**");
 	}
 
-	@Bean
-	public CorsConfigurationSource corsConfigurationSource() {
+	// @Override
+	// protected void configure(HttpSecurity httpSecurity) throws Exception {
+	// httpSecurity.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().cors().and()
+	// .csrf().disable().authorizeRequests().antMatchers("*").permitAll();
+	// }
 
-		CorsConfiguration corsConfiguration = new CorsConfiguration();
-		corsConfiguration.applyPermitDefaultValues();
-
-		corsConfiguration.addAllowedOrigin("*");
-		corsConfiguration.addAllowedHeader("*");
-		corsConfiguration.addAllowedMethod(HttpMethod.OPTIONS);
-		corsConfiguration.addAllowedMethod(HttpMethod.HEAD);
-		corsConfiguration.addAllowedMethod(HttpMethod.GET);
-		corsConfiguration.addAllowedMethod(HttpMethod.PUT);
-		corsConfiguration.addAllowedMethod(HttpMethod.POST);
-		corsConfiguration.addAllowedMethod(HttpMethod.DELETE);
-		corsConfiguration.addAllowedMethod(HttpMethod.PATCH);
-
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", corsConfiguration);
-
-		return source;
-	}
+	// @Bean
+	// public CorsConfigurationSource corsConfigurationSource() {
+	//
+	// CorsConfiguration corsConfiguration = new CorsConfiguration();
+	// corsConfiguration.applyPermitDefaultValues();
+	//
+	// corsConfiguration.addAllowedOrigin("*");
+	// corsConfiguration.addAllowedHeader("*");
+	// corsConfiguration.addAllowedMethod(HttpMethod.OPTIONS);
+	// corsConfiguration.addAllowedMethod(HttpMethod.HEAD);
+	// corsConfiguration.addAllowedMethod(HttpMethod.GET);
+	// corsConfiguration.addAllowedMethod(HttpMethod.PUT);
+	// corsConfiguration.addAllowedMethod(HttpMethod.POST);
+	// corsConfiguration.addAllowedMethod(HttpMethod.DELETE);
+	// corsConfiguration.addAllowedMethod(HttpMethod.PATCH);
+	//
+	// UrlBasedCorsConfigurationSource source = new
+	// UrlBasedCorsConfigurationSource();
+	// source.registerCorsConfiguration("/**", corsConfiguration);
+	//
+	// return source;
+	// }
 }
--- src/main/java/com/backend/sprint/SprintApplication.java
+++ src/main/java/com/backend/sprint/SprintApplication.java
@@ -9,7 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.Ordered;
-import org.springframework.http.HttpMethod;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -36,25 +35,26 @@ public class SprintApplication {
 		return bean;
 	}
 
-	@Bean
-	public CorsFilter corsConfigurationSource() {
-
-		CorsConfiguration corsConfiguration = new CorsConfiguration();
-		corsConfiguration.applyPermitDefaultValues();
-
-		corsConfiguration.addAllowedOrigin("*");
-		corsConfiguration.addAllowedHeader("*");
-		corsConfiguration.addAllowedMethod(HttpMethod.OPTIONS);
-		corsConfiguration.addAllowedMethod(HttpMethod.HEAD);
-		corsConfiguration.addAllowedMethod(HttpMethod.GET);
-		corsConfiguration.addAllowedMethod(HttpMethod.PUT);
-		corsConfiguration.addAllowedMethod(HttpMethod.POST);
-		corsConfiguration.addAllowedMethod(HttpMethod.DELETE);
-		corsConfiguration.addAllowedMethod(HttpMethod.PATCH);
-
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", corsConfiguration);
-
-		return new CorsFilter(source);
-	}
+	// @Bean
+	// public CorsFilter corsConfigurationSource() {
+	//
+	// CorsConfiguration corsConfiguration = new CorsConfiguration();
+	// corsConfiguration.applyPermitDefaultValues();
+	//
+	// corsConfiguration.addAllowedOrigin("*");
+	// corsConfiguration.addAllowedHeader("*");
+	// corsConfiguration.addAllowedMethod(HttpMethod.OPTIONS);
+	// corsConfiguration.addAllowedMethod(HttpMethod.HEAD);
+	// corsConfiguration.addAllowedMethod(HttpMethod.GET);
+	// corsConfiguration.addAllowedMethod(HttpMethod.PUT);
+	// corsConfiguration.addAllowedMethod(HttpMethod.POST);
+	// corsConfiguration.addAllowedMethod(HttpMethod.DELETE);
+	// corsConfiguration.addAllowedMethod(HttpMethod.PATCH);
+	//
+	// UrlBasedCorsConfigurationSource source = new
+	// UrlBasedCorsConfigurationSource();
+	// source.registerCorsConfiguration("/**", corsConfiguration);
+	//
+	// return new CorsFilter(source);
+	// }
 }
