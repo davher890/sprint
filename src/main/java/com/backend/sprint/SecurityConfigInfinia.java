7c7a6486e260ad00633b437c4fc350ffc22d0bad
David <david@MacBook-Pro-de-David-2.local>
2020-07-14 18:34:25 +0200

    Heroku CORS get

--- src/main/java/com/backend/sprint/SecurityConfigInfinia.java
+++ src/main/java/com/backend/sprint/SecurityConfigInfinia.java
@@ -3,8 +3,10 @@ package com.backend.sprint;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -14,6 +16,14 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 @EnableWebSecurity
 public class SecurityConfigInfinia extends WebSecurityConfigurerAdapter {
+
+	private static final String[] SWAGGER_URL = { "/herokuapp**" };
+
+	@Override
+	protected void configure(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().cors().and()
+				.csrf().disable().authorizeRequests().antMatchers(SWAGGER_URL).permitAll();
+	}
 
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
