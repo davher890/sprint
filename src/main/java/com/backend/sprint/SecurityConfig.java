8636e98565609fb9e42fab998caee4446e054538
David <david@MacBook-Pro-de-David-2.local>
2020-11-05 18:00:41 +0100

    Fix CORS

--- src/main/java/com/backend/sprint/SecurityConfig.java
+++ src/main/java/com/backend/sprint/SecurityConfig.java
@@ -1,22 +1,16 @@
 package com.backend.sprint;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
-import com.backend.sprint.service.UserDetailsServiceImpl;
 
 @Configuration
 @EnableWebMvc
@@ -27,11 +21,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	private static final String LOGIN_URL = "/login/**";
 	private static final String USER_URL = "/users";
 
-	@Autowired
-	UserDetailsServiceImpl userDetailsService;
-
-	@Autowired
-	BCryptPasswordEncoder bCryptPasswordEncoder;
+	// @Autowired
+	// UserDetailsServiceImpl userDetailsService;
+	//
+	// @Autowired
+	// BCryptPasswordEncoder bCryptPasswordEncoder;
 
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
@@ -46,20 +40,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		// .addFilter(new JWTAuthorizationFilter(authenticationManager()));
 	}
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
-	}
-
-	@Override
-	public AuthenticationManager authenticationManagerBean() throws Exception {
-		return super.authenticationManagerBean();
-	}
-
-	@Bean
-	public BCryptPasswordEncoder getEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+	// @Override
+	// protected void configure(AuthenticationManagerBuilder auth) throws
+	// Exception {
+	// auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+	// }
+	//
+	// @Override
+	// public AuthenticationManager authenticationManagerBean() throws Exception
+	// {
+	// return super.authenticationManagerBean();
+	// }
+	//
+	// @Bean
+	// public BCryptPasswordEncoder getEncoder() {
+	// return new BCryptPasswordEncoder();
+	// }
 
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
