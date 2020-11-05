fed8204e140ec9ad174ee2fdfa6f3a9f63ca4bbc
David <david@MacBook-Pro-de-David-2.local>
2020-11-05 18:18:59 +0100

    Fix CORS

--- src/main/java/com/backend/sprint/SecurityConfig.java
+++ src/main/java/com/backend/sprint/SecurityConfig.java
@@ -1,20 +1,11 @@
 package com.backend.sprint;
 
-import java.util.Arrays;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@EnableWebMvc
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -30,8 +21,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
-		httpSecurity.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().cors().and()
-				.csrf().disable().authorizeRequests().antMatchers(SWAGGER_URL).permitAll();
+		httpSecurity// .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+				.cors().and().csrf().disable().authorizeRequests().antMatchers(SWAGGER_URL).permitAll();
 
 		// .authorizeRequests().antMatchers(LOGIN_URL).permitAll()
 		// .antMatchers(HttpMethod.POST, USER_URL).permitAll()
@@ -39,16 +30,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		// .authenticated().and()
 		// .addFilter(new JWTAuthenticationFilter(authenticationManager()))
 		// .addFilter(new JWTAuthorizationFilter(authenticationManager()));
-	}
-
-	@Bean
-	CorsConfigurationSource corsConfigurationSource() {
-		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(Arrays.asList("https://club-sprint-front.herokuapp.com"));
-		configuration.setAllowedMethods(Arrays.asList("GET", "POST"));
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", configuration);
-		return source;
 	}
 
 	// @Override
