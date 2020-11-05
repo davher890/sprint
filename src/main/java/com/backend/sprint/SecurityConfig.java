b8f2252b3bc188643ee6d78fb0246bd5b936c681
David <david@MacBook-Pro-de-David-2.local>
2020-11-05 18:41:37 +0100

    Fix CORS

--- src/main/java/com/backend/sprint/SecurityConfig.java
+++ src/main/java/com/backend/sprint/SecurityConfig.java
@@ -1,9 +1,14 @@
 package com.backend.sprint;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -22,7 +27,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
 		httpSecurity// .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-				.csrf().disable();
+				.cors().and().csrf().disable();
 		// .authorizeRequests()// .antMatchers(SWAGGER_URL).permitAll()
 		// .antMatchers(HttpMethod.OPTIONS, "**").permitAll();// allow CORS
 		// option
@@ -54,18 +59,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	// return new BCryptPasswordEncoder();
 	// }
 	//
-	// @Bean
-	// public CorsConfigurationSource corsConfigurationSource() {
-	//
-	// CorsConfiguration corsConfiguration = new CorsConfiguration();
-	// corsConfiguration.applyPermitDefaultValues();
-	// corsConfiguration.addAllowedMethod(HttpMethod.PUT);
-	// corsConfiguration.addAllowedMethod(HttpMethod.DELETE);
-	//
-	// UrlBasedCorsConfigurationSource source = new
-	// UrlBasedCorsConfigurationSource();
-	// source.registerCorsConfiguration("/**", corsConfiguration);
-	//
-	// return source;
-	// }
+
+	@Bean
+	public FilterRegistrationBean corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+		source.registerCorsConfiguration("/**", config);
+		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+		bean.setOrder(0);
+		return bean;
+	}
 }
