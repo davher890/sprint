db1044c29f16233f0c74778ab11ca7d2b8360d7a
David <david@MacBook-Pro-de-David-2.local>
2020-11-05 19:33:46 +0100

    Fix CORS

--- src/main/java/com/backend/sprint/SprintApplication.java
+++ src/main/java/com/backend/sprint/SprintApplication.java
@@ -1,11 +1,9 @@
 package com.backend.sprint;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableAutoConfiguration
 public class SprintApplication {
 
 	public static void main(String[] args) {
